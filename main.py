6f05b348e9f97ade6a7644d69eb534691c76d61b
juliana1thome <juju1thome@gmail.com>
2022-01-17 17:24:21 -0500

    Add post request to the project

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+# Note: in FastAPI the order matter. So, if you don't name your path it will take the first one that matches the request
+# being made
 
 # Creating an instance of fastapi
 app = FastAPI()
@@ -12,3 +15,14 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your retrived posts"}
+
+
+#Let's create posts
+@app.post("/create")
+# Body extracts all of the filds from the body, after it converts it to a python dict, and saves it into this variable
+# called body_data
+def create_posts(body_data: dict = Body(...)):
+    print(body_data) # Check the terminal for this print
+    return{"new_post": f"title: {body_data['title']} content: {body_data['content']}"}
+
+
