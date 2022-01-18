2af4a83e3dcc5c1e3c27ec028b7e774b7db5987b
juliana1thome <juju1thome@gmail.com>
2022-01-18 17:45:33 -0500

    Add get post by id

--- main.py
+++ main.py
@@ -14,12 +14,16 @@ my_posts =[{"title": "title of post 1", "content": "content of post 1", "id": 1}
 
 # FastAPI changes this masssage to JSON
 @app.get("/")
+
 def root():
+
     return{"message": "ʕ•́ᴥ•̀ʔっWelcome to Juliana's API"}
 
 # To retrive data use post request
 @app.get("/posts")
+
 def get_posts():
+
     return {"data": my_posts} # JSON Format
 
 
@@ -28,13 +32,23 @@ def get_posts():
 
 # Body extracts all of the filds from the body, after it converts it to a python dict, and saves it into this
 # class called Post that comes from the file post.py(which was a variable before, called body_data)
-# My Schema will be: title str, content str
 
 def create_posts(post: Post):
+
     # print(post) # Check the terminal for this print
     # print (post.dict()) # Transforming my pydantic model into a dict
     post_dict = post.dict()
+
     # Make this new id key have random value
     post_dict['id'] = randrange(0, 100000000)
     my_posts.append(post_dict)
+
     return{"data": post_dict}
+
+
+# Retriving one individual post
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+
+    return {"post_detail": f"This is the post {id}"}
